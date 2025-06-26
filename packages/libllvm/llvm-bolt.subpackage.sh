05698b5eb7d0f9716aeba915d89daff7ec370909
bibi09456 <55490658+bibi09456@users.noreply.github.com>
2025-06-26 18:12:46 +0700

    Create llvm-bolt.subpackage.sh

---
+++ packages/libllvm/llvm-bolt.subpackage.sh
@@ -0,0 +1,9 @@
+TERMUX_SUBPKG_DESCRIPTION="Post-link optimizer"
+TERMUX_SUBPKG_INCLUDE="
+bin/llvm-bolt*
+bin/llvm-bolt-heatmap*
+bin/llvm-boltdiff*
+bin/merge-fdata*
+bin/perf2bolt*
+lib/libbolt*.a
+"
